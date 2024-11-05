cce24f8043ba9e02c5dc2591c25b24a5e9c9e9cb
nagahiro6 <bunkyodaidaze@gmail.com>
2024-11-05 20:36:33 +0900

    Fix:F＿Ms＿CrtTor00　ﾌｫｰﾑ修正

---
+++ F_Ms_CrtTor00.designer.vb
@@ -0,0 +1,409 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class F_Ms_CrtTor00
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(F_Ms_CrtTor00))
+        Me.Cmd01 = New System.Windows.Forms.Button()
+        Me.Cmd02 = New System.Windows.Forms.Button()
+        Me.Cmd03 = New System.Windows.Forms.Button()
+        Me.Cmd10 = New System.Windows.Forms.Button()
+        Me.Cmd09 = New System.Windows.Forms.Button()
+        Me.Cmd08 = New System.Windows.Forms.Button()
+        Me.Cmd07 = New System.Windows.Forms.Button()
+        Me.Cmd06 = New System.Windows.Forms.Button()
+        Me.Cmd05 = New System.Windows.Forms.Button()
+        Me.Cmd04 = New System.Windows.Forms.Button()
+        Me.Cmd11 = New System.Windows.Forms.Button()
+        Me.Cmd12 = New System.Windows.Forms.Button()
+        Me.txtmsg = New System.Windows.Forms.TextBox()
+        Me.GroupBox00 = New System.Windows.Forms.GroupBox()
+        Me.La作成場所ICS = New System.Windows.Forms.Label()
+        Me.txt作成場所ICS科目残高 = New System.Windows.Forms.TextBox()
+        Me.La作成場所支払 = New System.Windows.Forms.Label()
+        Me.La作成場所カタリスト = New System.Windows.Forms.Label()
+        Me.txt作成場所支払座科目残高 = New System.Windows.Forms.TextBox()
+        Me.txt作成場所カタリスト科目残高 = New System.Windows.Forms.TextBox()
+        Me.btn参照3 = New System.Windows.Forms.Button()
+        Me.btn参照2 = New System.Windows.Forms.Button()
+        Me.btn参照1 = New System.Windows.Forms.Button()
+        Me.showSearch0 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox00.SuspendLayout()
+        CType(Me.showSearch0, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Cmd01
+        '
+        Me.Cmd01.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd01.Location = New System.Drawing.Point(4, 3)
+        Me.Cmd01.Name = "Cmd01"
+        Me.Cmd01.Size = New System.Drawing.Size(84, 36)
+        Me.Cmd01.TabIndex = 1
+        Me.Cmd01.Text = "F1 作成"
+        Me.Cmd01.UseVisualStyleBackColor = True
+        '
+        'Cmd02
+        '
+        Me.Cmd02.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd02.Location = New System.Drawing.Point(87, 3)
+        Me.Cmd02.Name = "Cmd02"
+        Me.Cmd02.Size = New System.Drawing.Size(84, 36)
+        Me.Cmd02.TabIndex = 31
+        Me.Cmd02.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Cmd02.UseVisualStyleBackColor = True
+        '
+        'Cmd03
+        '
+        Me.Cmd03.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd03.Location = New System.Drawing.Point(170, 3)
+        Me.Cmd03.Name = "Cmd03"
+        Me.Cmd03.Size = New System.Drawing.Size(84, 36)
+        Me.Cmd03.TabIndex = 32
+        Me.Cmd03.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Cmd03.UseVisualStyleBackColor = True
+        '
+        'Cmd10
+        '
+        Me.Cmd10.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd10.Location = New System.Drawing.Point(758, 3)
+        Me.Cmd10.Name = "Cmd10"
+        Me.Cmd10.Size = New System.Drawing.Size(84, 36)
+        Me.Cmd10.TabIndex = 2
+        Me.Cmd10.Text = "F10 終了"
+        Me.Cmd10.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Cmd10.UseVisualStyleBackColor = True
+        '
+        'Cmd09
+        '
+        Me.Cmd09.Font = New System.Drawing.Font("メイリオ", 9.75!)
+        Me.Cmd09.Location = New System.Drawing.Point(675, 3)
+        Me.Cmd09.Name = "Cmd09"
+        Me.Cmd09.Size = New System.Drawing.Size(84, 36)
+        Me.Cmd09.TabIndex = 9
+        Me.Cmd09.TabStop = False
+        Me.Cmd09.UseVisualStyleBackColor = True
+        '
+        'Cmd08
+        '
+        Me.Cmd08.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd08.Location = New System.Drawing.Point(588, 3)
+        Me.Cmd08.Name = "Cmd08"
+        Me.Cmd08.Size = New System.Drawing.Size(84, 36)
+        Me.Cmd08.TabIndex = 35
+        Me.Cmd08.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Cmd08.UseVisualStyleBackColor = True
+        '
+        'Cmd07
+        '
+        Me.Cmd07.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd07.Location = New System.Drawing.Point(505, 3)
+        Me.Cmd07.Name = "Cmd07"
+        Me.Cmd07.Size = New System.Drawing.Size(84, 36)
+        Me.Cmd07.TabIndex = 36
+        Me.Cmd07.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Cmd07.UseVisualStyleBackColor = True
+        '
+        'Cmd06
+        '
+        Me.Cmd06.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd06.Location = New System.Drawing.Point(422, 3)
+        Me.Cmd06.Name = "Cmd06"
+        Me.Cmd06.Size = New System.Drawing.Size(84, 36)
+        Me.Cmd06.TabIndex = 37
+        Me.Cmd06.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Cmd06.UseVisualStyleBackColor = True
+        '
+        'Cmd05
+        '
+        Me.Cmd05.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd05.Location = New System.Drawing.Point(339, 3)
+        Me.Cmd05.Name = "Cmd05"
+        Me.Cmd05.Size = New System.Drawing.Size(84, 36)
+        Me.Cmd05.TabIndex = 8
+        Me.Cmd05.TabStop = False
+        Me.Cmd05.UseVisualStyleBackColor = True
+        '
+        'Cmd04
+        '
+        Me.Cmd04.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd04.ForeColor = System.Drawing.Color.Red
+        Me.Cmd04.Location = New System.Drawing.Point(253, 3)
+        Me.Cmd04.Name = "Cmd04"
+        Me.Cmd04.Size = New System.Drawing.Size(84, 36)
+        Me.Cmd04.TabIndex = 39
+        Me.Cmd04.UseVisualStyleBackColor = True
+        '
+        'Cmd11
+        '
+        Me.Cmd11.Font = New System.Drawing.Font("メイリオ", 9.75!)
+        Me.Cmd11.Location = New System.Drawing.Point(841, 3)
+        Me.Cmd11.Name = "Cmd11"
+        Me.Cmd11.Size = New System.Drawing.Size(84, 36)
+        Me.Cmd11.TabIndex = 10
+        Me.Cmd11.TabStop = False
+        Me.Cmd11.UseVisualStyleBackColor = True
+        '
+        'Cmd12
+        '
+        Me.Cmd12.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd12.Location = New System.Drawing.Point(924, 3)
+        Me.Cmd12.Name = "Cmd12"
+        Me.Cmd12.Size = New System.Drawing.Size(84, 36)
+        Me.Cmd12.TabIndex = 40
+        Me.Cmd12.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Cmd12.UseVisualStyleBackColor = True
+        '
+        'txtmsg
+        '
+        Me.txtmsg.ForeColor = System.Drawing.Color.Red
+        Me.txtmsg.Location = New System.Drawing.Point(8, 215)
+        Me.txtmsg.Name = "txtmsg"
+        Me.txtmsg.Size = New System.Drawing.Size(262, 19)
+        Me.txtmsg.TabIndex = 4
+        Me.txtmsg.Text = "エラーメッセージ"
+        '
+        'GroupBox00
+        '
+        Me.GroupBox00.Controls.Add(Me.PictureBox2)
+        Me.GroupBox00.Controls.Add(Me.PictureBox1)
+        Me.GroupBox00.Controls.Add(Me.showSearch0)
+        Me.GroupBox00.Controls.Add(Me.btn参照1)
+        Me.GroupBox00.Controls.Add(Me.btn参照2)
+        Me.GroupBox00.Controls.Add(Me.btn参照3)
+        Me.GroupBox00.Controls.Add(Me.txtmsg)
+        Me.GroupBox00.Controls.Add(Me.txt作成場所カタリスト科目残高)
+        Me.GroupBox00.Controls.Add(Me.txt作成場所支払座科目残高)
+        Me.GroupBox00.Controls.Add(Me.La作成場所カタリスト)
+        Me.GroupBox00.Controls.Add(Me.La作成場所支払)
+        Me.GroupBox00.Controls.Add(Me.La作成場所ICS)
+        Me.GroupBox00.Controls.Add(Me.txt作成場所ICS科目残高)
+        Me.GroupBox00.Location = New System.Drawing.Point(4, 35)
+        Me.GroupBox00.Name = "GroupBox00"
+        Me.GroupBox00.Size = New System.Drawing.Size(1004, 240)
+        Me.GroupBox00.TabIndex = 242
+        Me.GroupBox00.TabStop = False
+        '
+        'La作成場所ICS
+        '
+        Me.La作成場所ICS.BackColor = System.Drawing.Color.FromArgb(CType(CType(81, Byte), Integer), CType(CType(45, Byte), Integer), CType(CType(168, Byte), Integer))
+        Me.La作成場所ICS.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.La作成場所ICS.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.La作成場所ICS.Font = New System.Drawing.Font("メイリオ", 9.75!)
+        Me.La作成場所ICS.ForeColor = System.Drawing.Color.White
+        Me.La作成場所ICS.Location = New System.Drawing.Point(6, 35)
+        Me.La作成場所ICS.Name = "La作成場所ICS"
+        Me.La作成場所ICS.Size = New System.Drawing.Size(172, 27)
+        Me.La作成場所ICS.TabIndex = 338
+        Me.La作成場所ICS.Text = "作成場所:ICS"
+        Me.La作成場所ICS.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txt作成場所ICS科目残高
+        '
+        Me.txt作成場所ICS科目残高.Enabled = False
+        Me.txt作成場所ICS科目残高.Font = New System.Drawing.Font("メイリオ", 9.75!)
+        Me.txt作成場所ICS科目残高.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.txt作成場所ICS科目残高.Location = New System.Drawing.Point(179, 35)
+        Me.txt作成場所ICS科目残高.MaxLength = 30
+        Me.txt作成場所ICS科目残高.Name = "txt作成場所ICS科目残高"
+        Me.txt作成場所ICS科目残高.Size = New System.Drawing.Size(288, 27)
+        Me.txt作成場所ICS科目残高.TabIndex = 1
+        Me.txt作成場所ICS科目残高.Text = "科目残高"
+        '
+        'La作成場所支払
+        '
+        Me.La作成場所支払.BackColor = System.Drawing.Color.FromArgb(CType(CType(81, Byte), Integer), CType(CType(45, Byte), Integer), CType(CType(168, Byte), Integer))
+        Me.La作成場所支払.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.La作成場所支払.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.La作成場所支払.Font = New System.Drawing.Font("メイリオ", 9.75!)
+        Me.La作成場所支払.ForeColor = System.Drawing.Color.White
+        Me.La作成場所支払.Location = New System.Drawing.Point(6, 102)
+        Me.La作成場所支払.Name = "La作成場所支払"
+        Me.La作成場所支払.Size = New System.Drawing.Size(172, 27)
+        Me.La作成場所支払.TabIndex = 340
+        Me.La作成場所支払.Text = "作成場所:支払"
+        Me.La作成場所支払.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'La作成場所カタリスト
+        '
+        Me.La作成場所カタリスト.BackColor = System.Drawing.Color.FromArgb(CType(CType(81, Byte), Integer), CType(CType(45, Byte), Integer), CType(CType(168, Byte), Integer))
+        Me.La作成場所カタリスト.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.La作成場所カタリスト.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.La作成場所カタリスト.Font = New System.Drawing.Font("メイリオ", 9.75!)
+        Me.La作成場所カタリスト.ForeColor = System.Drawing.Color.White
+        Me.La作成場所カタリスト.Location = New System.Drawing.Point(6, 69)
+        Me.La作成場所カタリスト.Name = "La作成場所カタリスト"
+        Me.La作成場所カタリスト.Size = New System.Drawing.Size(172, 27)
+        Me.La作成場所カタリスト.TabIndex = 341
+        Me.La作成場所カタリスト.Text = "作成場所:カタリスト"
+        Me.La作成場所カタリスト.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txt作成場所支払座科目残高
+        '
+        Me.txt作成場所支払座科目残高.Enabled = False
+        Me.txt作成場所支払座科目残高.Font = New System.Drawing.Font("メイリオ", 9.75!)
+        Me.txt作成場所支払座科目残高.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.txt作成場所支払座科目残高.Location = New System.Drawing.Point(179, 102)
+        Me.txt作成場所支払座科目残高.MaxLength = 4
+        Me.txt作成場所支払座科目残高.Name = "txt作成場所支払座科目残高"
+        Me.txt作成場所支払座科目残高.Size = New System.Drawing.Size(288, 27)
+        Me.txt作成場所支払座科目残高.TabIndex = 5
+        Me.txt作成場所支払座科目残高.Text = "科目残高"
+        '
+        'txt作成場所カタリスト科目残高
+        '
+        Me.txt作成場所カタリスト科目残高.Enabled = False
+        Me.txt作成場所カタリスト科目残高.Font = New System.Drawing.Font("メイリオ", 9.75!)
+        Me.txt作成場所カタリスト科目残高.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.txt作成場所カタリスト科目残高.Location = New System.Drawing.Point(179, 69)
+        Me.txt作成場所カタリスト科目残高.MaxLength = 30
+        Me.txt作成場所カタリスト科目残高.Name = "txt作成場所カタリスト科目残高"
+        Me.txt作成場所カタリスト科目残高.Size = New System.Drawing.Size(288, 27)
+        Me.txt作成場所カタリスト科目残高.TabIndex = 3
+        Me.txt作成場所カタリスト科目残高.Text = "科目残高"
+        '
+        'btn参照3
+        '
+        Me.btn参照3.Font = New System.Drawing.Font("メイリオ", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btn参照3.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.btn参照3.Location = New System.Drawing.Point(476, 102)
+        Me.btn参照3.Name = "btn参照3"
+        Me.btn参照3.Size = New System.Drawing.Size(93, 27)
+        Me.btn参照3.TabIndex = 6
+        Me.btn参照3.Text = "参照"
+        Me.btn参照3.UseVisualStyleBackColor = True
+        '
+        'btn参照2
+        '
+        Me.btn参照2.Font = New System.Drawing.Font("メイリオ", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btn参照2.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.btn参照2.Location = New System.Drawing.Point(476, 69)
+        Me.btn参照2.Name = "btn参照2"
+        Me.btn参照2.Size = New System.Drawing.Size(93, 27)
+        Me.btn参照2.TabIndex = 4
+        Me.btn参照2.Text = "参照"
+        Me.btn参照2.UseVisualStyleBackColor = True
+        '
+        'btn参照1
+        '
+        Me.btn参照1.Font = New System.Drawing.Font("メイリオ", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btn参照1.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.btn参照1.Location = New System.Drawing.Point(476, 35)
+        Me.btn参照1.Name = "btn参照1"
+        Me.btn参照1.Size = New System.Drawing.Size(93, 27)
+        Me.btn参照1.TabIndex = 2
+        Me.btn参照1.Text = "参照"
+        Me.btn参照1.UseVisualStyleBackColor = True
+        '
+        'showSearch0
+        '
+        Me.showSearch0.Image = CType(resources.GetObject("showSearch0.Image"), System.Drawing.Image)
+        Me.showSearch0.Location = New System.Drawing.Point(481, 106)
+        Me.showSearch0.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.showSearch0.Name = "showSearch0"
+        Me.showSearch0.Size = New System.Drawing.Size(18, 20)
+        Me.showSearch0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.showSearch0.TabIndex = 596
+        Me.showSearch0.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(481, 73)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(18, 20)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 597
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(481, 39)
+        Me.PictureBox2.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(18, 20)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 598
+        Me.PictureBox2.TabStop = False
+        '
+        'F_Ms_CrtTor00
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1013, 280)
+        Me.Controls.Add(Me.Cmd12)
+        Me.Controls.Add(Me.Cmd11)
+        Me.Controls.Add(Me.Cmd10)
+        Me.Controls.Add(Me.Cmd08)
+        Me.Controls.Add(Me.Cmd07)
+        Me.Controls.Add(Me.Cmd06)
+        Me.Controls.Add(Me.Cmd04)
+        Me.Controls.Add(Me.Cmd05)
+        Me.Controls.Add(Me.Cmd09)
+        Me.Controls.Add(Me.Cmd03)
+        Me.Controls.Add(Me.Cmd02)
+        Me.Controls.Add(Me.Cmd01)
+        Me.Controls.Add(Me.GroupBox00)
+        Me.KeyPreview = True
+        Me.Name = "F_Ms_CrtTor00"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "取引先マスタ送信処理"
+        Me.GroupBox00.ResumeLayout(False)
+        Me.GroupBox00.PerformLayout()
+        CType(Me.showSearch0, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Cmd01 As System.Windows.Forms.Button
+    Friend WithEvents Cmd02 As System.Windows.Forms.Button
+    Friend WithEvents Cmd03 As System.Windows.Forms.Button
+    Friend WithEvents Cmd10 As System.Windows.Forms.Button
+    Friend WithEvents Cmd09 As System.Windows.Forms.Button
+    Friend WithEvents Cmd08 As System.Windows.Forms.Button
+    Friend WithEvents Cmd07 As System.Windows.Forms.Button
+    Friend WithEvents Cmd06 As System.Windows.Forms.Button
+    Friend WithEvents Cmd05 As System.Windows.Forms.Button
+    Friend WithEvents Cmd04 As System.Windows.Forms.Button
+    Friend WithEvents Cmd11 As System.Windows.Forms.Button
+    Friend WithEvents Cmd12 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox00 As System.Windows.Forms.GroupBox
+    Friend WithEvents txt作成場所ICS科目残高 As System.Windows.Forms.TextBox
+    Friend WithEvents La作成場所ICS As Label
+    Friend WithEvents txtmsg As TextBox
+    Friend WithEvents txt作成場所カタリスト科目残高 As TextBox
+    Friend WithEvents txt作成場所支払座科目残高 As TextBox
+    Friend WithEvents La作成場所カタリスト As Label
+    Friend WithEvents La作成場所支払 As Label
+    Friend WithEvents btn参照1 As Button
+    Friend WithEvents btn参照2 As Button
+    Friend WithEvents btn参照3 As Button
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents showSearch0 As PictureBox
+End Class
