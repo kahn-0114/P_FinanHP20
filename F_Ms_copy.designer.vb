8b0d91135e8548344a2e13d2f2666ee5ef499871
kahn-0114 <157479198+kahn-0114@users.noreply.github.com>
2024-11-05 20:21:52 +0900

    画像

---
+++ F_Ms_copy.designer.vb
@@ -0,0 +1,238 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class F_Ms_素材
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(F_Ms_素材))
+        Me.Cmd09 = New System.Windows.Forms.Button()
+        Me.Cmd10 = New System.Windows.Forms.Button()
+        Me.Cmd11 = New System.Windows.Forms.Button()
+        Me.Cmd12 = New System.Windows.Forms.Button()
+        Me.Cmd04 = New System.Windows.Forms.Button()
+        Me.Cmd05 = New System.Windows.Forms.Button()
+        Me.Cmd06 = New System.Windows.Forms.Button()
+        Me.Cmd07 = New System.Windows.Forms.Button()
+        Me.Cmd08 = New System.Windows.Forms.Button()
+        Me.Cmd03 = New System.Windows.Forms.Button()
+        Me.Cmd02 = New System.Windows.Forms.Button()
+        Me.Cmd01 = New System.Windows.Forms.Button()
+        Me.showSearch0 = New System.Windows.Forms.PictureBox()
+        CType(Me.showSearch0, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Cmd09
+        '
+        Me.Cmd09.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd09.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd09.Location = New System.Drawing.Point(664, 1)
+        Me.Cmd09.Name = "Cmd09"
+        Me.Cmd09.Size = New System.Drawing.Size(80, 45)
+        Me.Cmd09.TabIndex = 53
+        Me.Cmd09.TabStop = False
+        Me.Cmd09.UseVisualStyleBackColor = False
+        '
+        'Cmd10
+        '
+        Me.Cmd10.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd10.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd10.Location = New System.Drawing.Point(743, 1)
+        Me.Cmd10.Name = "Cmd10"
+        Me.Cmd10.Size = New System.Drawing.Size(80, 45)
+        Me.Cmd10.TabIndex = 52
+        Me.Cmd10.TabStop = False
+        Me.Cmd10.UseVisualStyleBackColor = False
+        '
+        'Cmd11
+        '
+        Me.Cmd11.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd11.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd11.ForeColor = System.Drawing.Color.Black
+        Me.Cmd11.Location = New System.Drawing.Point(822, 1)
+        Me.Cmd11.Name = "Cmd11"
+        Me.Cmd11.Size = New System.Drawing.Size(80, 45)
+        Me.Cmd11.TabIndex = 51
+        Me.Cmd11.TabStop = False
+        Me.Cmd11.Text = "F11終了"
+        Me.Cmd11.UseVisualStyleBackColor = False
+        '
+        'Cmd12
+        '
+        Me.Cmd12.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd12.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd12.Location = New System.Drawing.Point(901, 1)
+        Me.Cmd12.Name = "Cmd12"
+        Me.Cmd12.Size = New System.Drawing.Size(80, 45)
+        Me.Cmd12.TabIndex = 50
+        Me.Cmd12.TabStop = False
+        Me.Cmd12.UseVisualStyleBackColor = False
+        '
+        'Cmd04
+        '
+        Me.Cmd04.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd04.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd04.ForeColor = System.Drawing.Color.White
+        Me.Cmd04.Location = New System.Drawing.Point(250, 1)
+        Me.Cmd04.Name = "Cmd04"
+        Me.Cmd04.Size = New System.Drawing.Size(85, 45)
+        Me.Cmd04.TabIndex = 49
+        Me.Cmd04.TabStop = False
+        Me.Cmd04.UseVisualStyleBackColor = False
+        '
+        'Cmd05
+        '
+        Me.Cmd05.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd05.FlatAppearance.BorderColor = System.Drawing.Color.RosyBrown
+        Me.Cmd05.FlatAppearance.BorderSize = 2
+        Me.Cmd05.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd05.ForeColor = System.Drawing.Color.Black
+        Me.Cmd05.Location = New System.Drawing.Point(339, 1)
+        Me.Cmd05.Name = "Cmd05"
+        Me.Cmd05.Size = New System.Drawing.Size(85, 45)
+        Me.Cmd05.TabIndex = 48
+        Me.Cmd05.TabStop = False
+        Me.Cmd05.Text = "F5 " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "入力取消"
+        Me.Cmd05.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Cmd05.UseVisualStyleBackColor = False
+        '
+        'Cmd06
+        '
+        Me.Cmd06.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd06.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd06.ForeColor = System.Drawing.Color.White
+        Me.Cmd06.Location = New System.Drawing.Point(423, 1)
+        Me.Cmd06.Name = "Cmd06"
+        Me.Cmd06.Size = New System.Drawing.Size(80, 45)
+        Me.Cmd06.TabIndex = 47
+        Me.Cmd06.TabStop = False
+        Me.Cmd06.UseVisualStyleBackColor = False
+        '
+        'Cmd07
+        '
+        Me.Cmd07.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd07.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd07.ForeColor = System.Drawing.Color.White
+        Me.Cmd07.Location = New System.Drawing.Point(502, 1)
+        Me.Cmd07.Name = "Cmd07"
+        Me.Cmd07.Size = New System.Drawing.Size(80, 45)
+        Me.Cmd07.TabIndex = 46
+        Me.Cmd07.TabStop = False
+        Me.Cmd07.UseVisualStyleBackColor = False
+        '
+        'Cmd08
+        '
+        Me.Cmd08.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd08.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd08.ForeColor = System.Drawing.Color.White
+        Me.Cmd08.Location = New System.Drawing.Point(581, 1)
+        Me.Cmd08.Name = "Cmd08"
+        Me.Cmd08.Size = New System.Drawing.Size(80, 45)
+        Me.Cmd08.TabIndex = 45
+        Me.Cmd08.TabStop = False
+        Me.Cmd08.UseVisualStyleBackColor = False
+        '
+        'Cmd03
+        '
+        Me.Cmd03.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd03.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd03.ForeColor = System.Drawing.Color.White
+        Me.Cmd03.Location = New System.Drawing.Point(167, 1)
+        Me.Cmd03.Name = "Cmd03"
+        Me.Cmd03.Size = New System.Drawing.Size(85, 45)
+        Me.Cmd03.TabIndex = 44
+        Me.Cmd03.TabStop = False
+        Me.Cmd03.UseVisualStyleBackColor = False
+        '
+        'Cmd02
+        '
+        Me.Cmd02.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd02.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd02.ForeColor = System.Drawing.Color.White
+        Me.Cmd02.Location = New System.Drawing.Point(83, 1)
+        Me.Cmd02.Name = "Cmd02"
+        Me.Cmd02.Size = New System.Drawing.Size(85, 45)
+        Me.Cmd02.TabIndex = 43
+        Me.Cmd02.TabStop = False
+        Me.Cmd02.UseVisualStyleBackColor = False
+        '
+        'Cmd01
+        '
+        Me.Cmd01.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd01.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd01.ForeColor = System.Drawing.Color.White
+        Me.Cmd01.Location = New System.Drawing.Point(-1, 1)
+        Me.Cmd01.Name = "Cmd01"
+        Me.Cmd01.Size = New System.Drawing.Size(85, 45)
+        Me.Cmd01.TabIndex = 42
+        Me.Cmd01.TabStop = False
+        Me.Cmd01.UseVisualStyleBackColor = False
+        '
+        'showSearch0
+        '
+        Me.showSearch0.Image = CType(resources.GetObject("showSearch0.Image"), System.Drawing.Image)
+        Me.showSearch0.Location = New System.Drawing.Point(317, 129)
+        Me.showSearch0.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.showSearch0.Name = "showSearch0"
+        Me.showSearch0.Size = New System.Drawing.Size(18, 20)
+        Me.showSearch0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.showSearch0.TabIndex = 595
+        Me.showSearch0.TabStop = False
+        '
+        'F_Ms_素材
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(982, 453)
+        Me.Controls.Add(Me.showSearch0)
+        Me.Controls.Add(Me.Cmd12)
+        Me.Controls.Add(Me.Cmd11)
+        Me.Controls.Add(Me.Cmd10)
+        Me.Controls.Add(Me.Cmd08)
+        Me.Controls.Add(Me.Cmd07)
+        Me.Controls.Add(Me.Cmd06)
+        Me.Controls.Add(Me.Cmd09)
+        Me.Controls.Add(Me.Cmd04)
+        Me.Controls.Add(Me.Cmd05)
+        Me.Controls.Add(Me.Cmd03)
+        Me.Controls.Add(Me.Cmd02)
+        Me.Controls.Add(Me.Cmd01)
+        Me.KeyPreview = True
+        Me.Name = "F_Ms_素材"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "F_Ms_素材"
+        CType(Me.showSearch0, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Cmd09 As System.Windows.Forms.Button
+    Friend WithEvents Cmd10 As System.Windows.Forms.Button
+    Friend WithEvents Cmd11 As System.Windows.Forms.Button
+    Friend WithEvents Cmd12 As System.Windows.Forms.Button
+    Friend WithEvents Cmd04 As System.Windows.Forms.Button
+    Friend WithEvents Cmd05 As System.Windows.Forms.Button
+    Friend WithEvents Cmd06 As System.Windows.Forms.Button
+    Friend WithEvents Cmd07 As System.Windows.Forms.Button
+    Friend WithEvents Cmd08 As System.Windows.Forms.Button
+    Friend WithEvents Cmd03 As System.Windows.Forms.Button
+    Friend WithEvents Cmd02 As System.Windows.Forms.Button
+    Friend WithEvents Cmd01 As System.Windows.Forms.Button
+    Friend WithEvents showSearch0 As PictureBox
+End Class
---
+++ F_Ms_copy.vb
@@ -0,0 +1,403 @@
+﻿Imports System.Data.SqlClient
+Imports System.Windows.Forms
+Imports System.ComponentModel
+Public Class F_Ms_素材
+
+    Dim pFocus(10) As Object
+    Dim pubCom As New Com
+    Dim pubProc As New Proc
+    Private Mysqlserver As SQLServer = New SQLServer()
+    '******************************************************
+    '* リストにItemを表示する共通関数
+    '******************************************************
+    Private Sub Add_Item00()
+
+        ' SQLクエリの構築
+        Dim SQL As String = "SELECT 区分,区分名 FROM MM_区分H00 WHERE 会社No = @会社No ORDER BY 区分"
+
+        ' パラメータのリストを作成
+        Dim parameters As New List(Of SqlParameter) From {
+            New SqlParameter("@会社No", pubComPany)
+            }
+
+        ' データベースからデータを取得
+        Dim result As DataTable = Mysqlserver.GetData(SQL, parameters.ToArray())
+
+        ' 取得したデータの処理
+        lv.Items.Clear()
+        If result IsNot Nothing AndAlso result.Rows.Count > 0 Then
+            Dim i0 As Integer = 0
+            For Each Row As DataRow In result.Rows
+                lv.Items.Add(Integer.Parse(Row("区分")).ToString("000"), i0)
+                lv.Items(i0).SubItems.Add(pubCom.ChgNull(Row("区分名"), 1))
+                i0 += 1
+            Next
+
+        End If
+        result.Dispose()
+
+    End Sub
+
+    '******************************************************
+    '*
+    '******************************************************
+    Public Sub Dsp_Init00()
+        For Each CtrlItem As Control In Me.GroupBox00.Controls
+            If TypeOf CtrlItem Is TextBox Then
+                AddHandler CtrlItem.Enter, AddressOf txtInp_Enter
+                AddHandler CtrlItem.KeyPress, AddressOf txtInp_KeyPress
+                AddHandler CtrlItem.KeyDown, AddressOf txtInp_KeyDown
+                AddHandler CtrlItem.Leave, AddressOf txtInp_Leave
+            End If
+        Next
+        For Each CtrlItem As Control In Me.GroupBox10.Controls
+            If TypeOf CtrlItem Is TextBox Then
+                AddHandler CtrlItem.Enter, AddressOf txtInp_Enter
+                AddHandler CtrlItem.KeyPress, AddressOf txtInp_KeyPress
+                AddHandler CtrlItem.KeyDown, AddressOf txtInp_KeyDown
+                AddHandler CtrlItem.Leave, AddressOf txtInp_Leave
+            End If
+        Next
+    End Sub
+    '******************************************************
+    '* フィールドクリア関数
+    '******************************************************
+    Private Sub Inp_Clr00()
+
+        For Each CtrlItem As Control In Me.GroupBox00.Controls
+            If TypeOf CtrlItem Is TextBox Then CtrlItem.Text = ""
+        Next
+        For Each CtrlItem As Control In Me.GroupBox10.Controls
+            If TypeOf CtrlItem Is TextBox Then CtrlItem.Text = ""
+        Next
+        lv.Items.Clear()
+        txtMsg.Text = "" : txtMsg.Visible = False
+    End Sub
+    Private Sub txtInp_Enter(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Dim pTxtBox As TextBox = CType(sender, TextBox)
+        If pTxtBox.ReadOnly = True Then Exit Sub
+
+        pTxtBox.SelectAll() : pTxtBox.BackColor = Color.MistyRose
+        txtModified.Text = ""
+        Select Case pTxtBox.Name
+            Case "txt区分CD"
+                AddHandler txt区分CD.TextChanged, AddressOf txtModified_TextChanged
+        End Select
+        pFocus(0) = pTxtBox
+    End Sub
+    Private Sub txtInp_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs)
+        Dim pTxtBox As TextBox = CType(sender, TextBox)
+
+        Select Case e.KeyCode
+            Case 38
+                Me.SelectNextControl(sender, False, True, True, True)
+                e.Handled = True
+        End Select
+    End Sub
+    Private Sub txtInp_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs)
+        Dim pTxtBox As TextBox = CType(sender, TextBox)
+        If pTxtBox.ReadOnly = True Then
+            Me.SelectNextControl(sender, True, True, True, True)
+            Exit Sub
+        End If
+
+        Select Case pTxtBox.Name
+            Case "txt区分CD"
+                Select Case e.KeyChar
+                    Case ChrW(48) To ChrW(57), ChrW(8), ChrW(13)
+                    Case Else
+                        e.Handled = True
+                End Select
+            Case "txt桁数"
+                Select Case e.KeyChar
+                    Case ChrW(48) To ChrW(52), ChrW(8), ChrW(13)
+                    Case Else
+                        e.Handled = True
+                End Select
+        End Select
+
+        Select Case e.KeyChar
+            Case Chr(Keys.Enter)
+                Dim pRet As Boolean = Inp_Chk00(sender)
+                If pRet = False Then Exit Sub
+                Me.SelectNextControl(sender, True, True, True, True)
+                e.Handled = True
+        End Select
+    End Sub
+    Private Sub txtInp_Leave(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Dim pTxtBox As TextBox = CType(sender, TextBox)
+        If pTxtBox.ReadOnly = True Then Exit Sub
+
+        Select Case pTxtBox.Name
+            Case "txt区分CD"
+                If txtModified.Text = "1" Then
+                    btnOK00.PerformClick()
+                End If
+                RemoveHandler txt区分CD.TextChanged, AddressOf txtModified_TextChanged
+        End Select
+        txtModified.Text = ""
+        pTxtBox.BackColor = Color.White
+    End Sub
+
+    Private Sub txtModified_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs)
+        txtModified.Text = "1"
+    End Sub
+
+    Private Sub Cmd11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cmd11.Click
+        Me.Close()
+    End Sub
+
+
+
+    Private Sub F_Ms_KubunH00_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+        Select Case e.KeyCode
+            Case 116
+                Cmd05.PerformClick() : e.Handled = True
+            Case 122
+                Cmd11.PerformClick() : e.Handled = True
+        End Select
+    End Sub
+
+    Private Sub F_Ms_KubunH00_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Inp_Clr00()
+        Dsp_Init00()
+        Add_Item00()
+        Me.ActiveControl = Me.txt区分CD
+    End Sub
+
+    '******************************************************
+    '* GroupBox10設定
+    '******************************************************
+    Private Sub btnOK00_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Dim pRet As Boolean = Inp_Chk00(txt区分CD)
+        If pRet = False Then Exit Sub
+        Data_Get00()
+        txt区分名.Focus()
+    End Sub
+
+    '******************************************************
+    '* 入力項目ﾁｪｯｸ
+    '******************************************************
+    Private Function Inp_Chk00(ByVal ctxtInp As Control)
+        Inp_Chk00 = True
+        txtMsg.Visible = False
+        txtMsg.Text = ""
+
+        Select Case ctxtInp.Name
+            Case "txt区分CD"
+                ctxtInp.BackColor = Color.White
+                If txt区分CD.Text = "" Then
+                    txtMsg.Text = "区分CD未入力"
+                    Inp_Chk00 = False : txtMsg.Visible = True : ctxtInp.BackColor = Color.LightCoral : Exit Function
+                End If
+            Case "txt区分名"
+                ctxtInp.BackColor = Color.White
+                Dim LenB As Integer = System.Text.Encoding.GetEncoding("Shift_JIS").GetByteCount(ctxtInp.Text)
+                If LenB > 40 Then
+                    txtMsg.Text = "区分名超過:40"
+                    Inp_Chk00 = False : txtMsg.Visible = True : ctxtInp.BackColor = Color.LightCoral : Exit Function
+                End If
+            Case "txt桁数"
+                ctxtInp.BackColor = Color.White
+                If Not IsNumeric(ctxtInp.Text) Then
+                    ctxtInp.Text = 3
+                End If
+        End Select
+    End Function
+
+    '******************************************************
+    '* MM_区分H00削除
+    '******************************************************
+    Public Sub Data_Del00()
+
+        ' SQLクエリの構築
+        Dim condition As String = "会社No = @会社No " &
+                                     "AND 区分 = @区分"
+
+        Dim parameters As New List(Of SqlParameter) From {
+        New SqlParameter("@会社No", pubComPany),
+        New SqlParameter("@区分", txt区分CD.Text)
+    }
+
+        Mysqlserver.DeleteData("MM_区分H00", condition, parameters.ToArray())
+    End Sub
+
+
+
+    '******************************************************
+    '* MM_区分H00取得
+    '******************************************************
+    Public Sub Data_Get00()
+        'Dim da0 As New SqlDataAdapter, dt0 As New DataSet
+        Dim SQL As String
+        SQL = "SELECT * " _
+            & "FROM MM_区分H00 " _
+            & "WHERE 会社No = @会社No " _
+            & "AND 区分 = @区分"
+        Dim parameters As New List(Of SqlParameter) From {
+                   New SqlParameter("@会社No", pubComPany),
+                   New SqlParameter("@区分", txt区分CD.Text)
+                   }
+        Dim result As DataTable = Mysqlserver.GetData(SQL, parameters.ToArray())
+        If result.Rows.Count > 0 Then
+            txt区分名.Text = pubCom.ChgNull(result.Rows(0)("区分名"), 1)
+            RaType0.Checked = True
+            If pubCom.ChgNull(result.Rows(0)("Type"), 0) = 2 Then
+                RaType1.Checked = True
+            End If
+            txt桁数.Text = pubCom.ChgNull(result.Rows(0)("桁数"), 0)
+            RaUp0.Checked = True
+            If pubCom.ChgNull(result.Rows(0)("更新区分"), 0) = 9 Then
+                RaUp1.Checked = True
+            End If
+
+        End If
+        result.Dispose()
+    End Sub
+    '******************************************************
+    '* MM_区分H00保存
+    '******************************************************
+    Public Sub Data_Put00()
+        Dim SQL As String
+
+        SQL = " SELECT * " &
+              " FROM MM_区分H00 " &
+              " WHERE 会社No = @会社No" &
+              " And 区分= @区分"
+        Dim wtype As String = 1
+        If RaType1.Checked = True Then
+            wtype = 2
+        End If
+        Dim wUp As String = 0
+        If RaUp1.Checked = True Then
+            wUp = 9
+        End If
+        ' パラメータのリストを作成
+        Dim parameters As New List(Of SqlParameter) From {
+                   New SqlParameter("@会社No", pubComPany),
+                   New SqlParameter("@区分", txt区分CD.Text),
+                   New SqlParameter("@Type", wtype),
+                   New SqlParameter("@桁数", txt桁数.Text),
+                   New SqlParameter("@更新区分", wUp),
+                   New SqlParameter("@削除", 0)
+       }
+        ' データベースからデータを取得
+        Dim result As DataTable = Mysqlserver.GetData(SQL, parameters.ToArray())
+        'For i As Integer = 0 To result.Rows.Count - 1
+        parameters.Clear()
+        parameters.Add(New SqlParameter("@会社No", pubComPany))
+        parameters.Add(New SqlParameter("@区分", txt区分CD.Text))
+        parameters.Add(New SqlParameter("@区分名", txt区分名.Text))
+        parameters.Add(New SqlParameter("@Type", wtype))
+        parameters.Add(New SqlParameter("@桁数", txt桁数.Text))
+        parameters.Add(New SqlParameter("@更新区分", wUp))
+        parameters.Add(New SqlParameter("@削除", 0))
+
+        If result.Rows.Count = 0 Then
+            Dim columns As String = "会社No " &
+                                    ", 区分 " &
+                                    ", 区分名 " &
+                                    ", Type " &
+                                    ", 桁数 " &
+                                    ", 更新区分 " &
+                                    ", 削除 "
+            ' 登録する値を指定
+            Dim values As String = "@会社No " &
+                                   ", @区分 " &
+                                   ", @区分名 " &
+                                   ", @Type " &
+                                   ", @桁数 " &
+                                   ", @更新区分 " &
+                                   ", @削除 "
+            Mysqlserver.InsertData("MM_区分H00", columns, values, parameters.ToArray())
+        Else
+            Dim setStatement As String = "区分名 = @区分名" &
+                                         ", Type = @Type " &
+                                         ", 桁数 = @桁数 " &
+                                         ", 更新区分 = @更新区分 " &
+                                         ", 削除 = @削除 "
+            ' 更新条件を指定
+            Dim condition As String = "会社No = @会社No " &
+                                      "AND 区分 = @区分 "
+            Mysqlserver.UpdateData("MM_区分H00", setStatement, condition, parameters.ToArray())
+        End If
+        result.Dispose()
+    End Sub
+
+    '******************************************************
+    '* 保存処理
+    '******************************************************
+    Private Sub btnUp00_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Dim pRet As Boolean = Inp_Chk00(txt区分CD)
+        If pRet = False Then Exit Sub
+        For Each CtrlItem As Control In Me.GroupBox10.Controls
+            If TypeOf CtrlItem Is TextBox Then
+                pRet = Inp_Chk00(CtrlItem)
+                If pRet = False Then Exit Sub
+            End If
+        Next
+
+        Data_Put00()
+        Inp_Clr00()
+        Add_Item00()
+        txt区分CD.Focus()
+    End Sub
+
+    '******************************************************
+    '* 削除処理
+    '******************************************************
+    Private Sub btnDel00_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Dim pRet As Boolean = Inp_Chk00(txt区分CD)
+        If pRet = False Then Exit Sub
+
+        Dim pMsg As String = "CD:" & txt区分CD.Text & Chr(13) _
+            & "名称:" & txt区分名.Text & Chr(13) _
+            & "このデータをマスタから削除します。"
+        Dim pAns As Integer = MsgBox(pMsg, vbYesNo + vbQuestion, "データ削除")
+        If pAns = vbNo Then Exit Sub
+
+        Data_Del00()
+        Inp_Clr00()
+        Add_Item00()
+        txt区分CD.Focus()
+    End Sub
+
+    Private Sub LV_ItemActivate(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Dim lv As ListView = DirectCast(sender, ListView)
+
+        txt区分CD.Text = lv.FocusedItem.Text
+        btnOK00.PerformClick()
+    End Sub
+
+    Private Sub Cmd05_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cmd05.Click
+        Inp_Clr00()
+        Add_Item00()
+        txt区分CD.Focus()
+    End Sub
+
+    Private Sub Cmd12_Click(sender As Object, e As EventArgs) Handles Cmd12.Click
+
+    End Sub
+
+    Private Sub Cmd06_Click(sender As Object, e As EventArgs) Handles Cmd06.Click
+
+    End Sub
+
+    Private Sub Cmd07_Click(sender As Object, e As EventArgs) Handles Cmd07.Click
+
+    End Sub
+
+    Private Sub Cmd08_Click(sender As Object, e As EventArgs) Handles Cmd08.Click
+
+    End Sub
+
+    Private Sub Cmd09_Click(sender As Object, e As EventArgs) Handles Cmd09.Click
+
+    End Sub
+
+    Private Sub Cmd10_Click(sender As Object, e As EventArgs) Handles Cmd10.Click
+
+    End Sub
+
+
+End Class
