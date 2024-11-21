3148c3ed8f5a4d33c18aaec4f908ed87c1910c4d
felulu427 <toba427@icloud.com>
2024-11-21 21:27:48 +0900

    F_An_PtIoLi00(マルチボードなし)完成

---
+++ F_An_PtIoLi00.vb
@@ -0,0 +1,17 @@
+﻿Public Class F_An_PtIoLi00
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txt年度.TextChanged
+
+    End Sub
+
+    Private Sub GroupBox10_Enter(sender As Object, e As EventArgs) Handles GroupBox00.Enter
+
+    End Sub
+
+    Private Sub txt対象終了日_TextChanged(sender As Object, e As EventArgs) Handles txt対象終了日.TextChanged
+
+    End Sub
+
+    Private Sub GroupBox10_Enter_1(sender As Object, e As EventArgs) Handles GroupBox10.Enter
+
+    End Sub
+End Class
