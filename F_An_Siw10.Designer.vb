c13b18b95dd101dc52c6a5819c2b288cef0b0c3b
felulu427 <toba427@icloud.com>
2024-11-21 20:13:26 +0900

    F_An_Siw10(マルチロールなし)完成

---
+++ F_An_Siw10.Designer.vb
@@ -0,0 +1,327 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class F_An_Siw10
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Cmd12 = New System.Windows.Forms.Button()
+        Me.Cmd11 = New System.Windows.Forms.Button()
+        Me.Cmd10 = New System.Windows.Forms.Button()
+        Me.Cmd08 = New System.Windows.Forms.Button()
+        Me.Cmd07 = New System.Windows.Forms.Button()
+        Me.Cmd06 = New System.Windows.Forms.Button()
+        Me.Cmd09 = New System.Windows.Forms.Button()
+        Me.Cmd04 = New System.Windows.Forms.Button()
+        Me.Cmd05 = New System.Windows.Forms.Button()
+        Me.Cmd03 = New System.Windows.Forms.Button()
+        Me.Cmd02 = New System.Windows.Forms.Button()
+        Me.Cmd01 = New System.Windows.Forms.Button()
+        Me.GroupBox10 = New System.Windows.Forms.GroupBox()
+        Me.btnSel00 = New System.Windows.Forms.Button()
+        Me.btnDel01 = New System.Windows.Forms.Button()
+        Me.GroupBox20 = New System.Windows.Forms.GroupBox()
+        Me.btnUP00 = New System.Windows.Forms.Button()
+        Me.btnDel00 = New System.Windows.Forms.Button()
+        Me.txtMsg = New System.Windows.Forms.TextBox()
+        Me.GroupBox10.SuspendLayout()
+        Me.GroupBox20.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Cmd12
+        '
+        Me.Cmd12.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd12.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd12.ForeColor = System.Drawing.Color.Black
+        Me.Cmd12.Location = New System.Drawing.Point(978, 12)
+        Me.Cmd12.Name = "Cmd12"
+        Me.Cmd12.Size = New System.Drawing.Size(88, 45)
+        Me.Cmd12.TabIndex = 98
+        Me.Cmd12.TabStop = False
+        Me.Cmd12.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Cmd12.UseVisualStyleBackColor = False
+        '
+        'Cmd11
+        '
+        Me.Cmd11.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd11.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd11.ForeColor = System.Drawing.Color.Black
+        Me.Cmd11.Location = New System.Drawing.Point(891, 12)
+        Me.Cmd11.Name = "Cmd11"
+        Me.Cmd11.Size = New System.Drawing.Size(88, 45)
+        Me.Cmd11.TabIndex = 99
+        Me.Cmd11.TabStop = False
+        Me.Cmd11.UseVisualStyleBackColor = False
+        '
+        'Cmd10
+        '
+        Me.Cmd10.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd10.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd10.ForeColor = System.Drawing.Color.Black
+        Me.Cmd10.Location = New System.Drawing.Point(804, 12)
+        Me.Cmd10.Name = "Cmd10"
+        Me.Cmd10.Size = New System.Drawing.Size(88, 45)
+        Me.Cmd10.TabIndex = 100
+        Me.Cmd10.TabStop = False
+        Me.Cmd10.Text = "F10 " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "終了"
+        Me.Cmd10.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Cmd10.UseVisualStyleBackColor = False
+        '
+        'Cmd08
+        '
+        Me.Cmd08.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd08.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Cmd08.ForeColor = System.Drawing.Color.Black
+        Me.Cmd08.Location = New System.Drawing.Point(621, 12)
+        Me.Cmd08.Name = "Cmd08"
+        Me.Cmd08.Size = New System.Drawing.Size(88, 45)
+        Me.Cmd08.TabIndex = 93
+        Me.Cmd08.TabStop = False
+        Me.Cmd08.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Cmd08.UseVisualStyleBackColor = False
+        '
+        'Cmd07
+        '
+        Me.Cmd07.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd07.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd07.ForeColor = System.Drawing.Color.White
+        Me.Cmd07.Location = New System.Drawing.Point(534, 12)
+        Me.Cmd07.Name = "Cmd07"
+        Me.Cmd07.Size = New System.Drawing.Size(88, 45)
+        Me.Cmd07.TabIndex = 94
+        Me.Cmd07.TabStop = False
+        Me.Cmd07.UseVisualStyleBackColor = False
+        '
+        'Cmd06
+        '
+        Me.Cmd06.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd06.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd06.ForeColor = System.Drawing.Color.White
+        Me.Cmd06.Location = New System.Drawing.Point(447, 12)
+        Me.Cmd06.Name = "Cmd06"
+        Me.Cmd06.Size = New System.Drawing.Size(88, 45)
+        Me.Cmd06.TabIndex = 95
+        Me.Cmd06.TabStop = False
+        Me.Cmd06.UseVisualStyleBackColor = False
+        '
+        'Cmd09
+        '
+        Me.Cmd09.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd09.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd09.Location = New System.Drawing.Point(717, 12)
+        Me.Cmd09.Name = "Cmd09"
+        Me.Cmd09.Size = New System.Drawing.Size(88, 45)
+        Me.Cmd09.TabIndex = 101
+        Me.Cmd09.TabStop = False
+        Me.Cmd09.UseVisualStyleBackColor = False
+        '
+        'Cmd04
+        '
+        Me.Cmd04.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd04.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd04.ForeColor = System.Drawing.Color.Black
+        Me.Cmd04.Location = New System.Drawing.Point(264, 12)
+        Me.Cmd04.Name = "Cmd04"
+        Me.Cmd04.Size = New System.Drawing.Size(88, 45)
+        Me.Cmd04.TabIndex = 97
+        Me.Cmd04.TabStop = False
+        Me.Cmd04.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Cmd04.UseVisualStyleBackColor = False
+        '
+        'Cmd05
+        '
+        Me.Cmd05.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd05.FlatAppearance.BorderColor = System.Drawing.Color.RosyBrown
+        Me.Cmd05.FlatAppearance.BorderSize = 2
+        Me.Cmd05.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Cmd05.ForeColor = System.Drawing.Color.Black
+        Me.Cmd05.Location = New System.Drawing.Point(360, 12)
+        Me.Cmd05.Name = "Cmd05"
+        Me.Cmd05.Size = New System.Drawing.Size(88, 45)
+        Me.Cmd05.TabIndex = 96
+        Me.Cmd05.TabStop = False
+        Me.Cmd05.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Cmd05.UseVisualStyleBackColor = False
+        '
+        'Cmd03
+        '
+        Me.Cmd03.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd03.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd03.ForeColor = System.Drawing.Color.White
+        Me.Cmd03.Location = New System.Drawing.Point(177, 12)
+        Me.Cmd03.Name = "Cmd03"
+        Me.Cmd03.Size = New System.Drawing.Size(88, 45)
+        Me.Cmd03.TabIndex = 92
+        Me.Cmd03.TabStop = False
+        Me.Cmd03.UseVisualStyleBackColor = False
+        '
+        'Cmd02
+        '
+        Me.Cmd02.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd02.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd02.ForeColor = System.Drawing.Color.White
+        Me.Cmd02.Location = New System.Drawing.Point(90, 12)
+        Me.Cmd02.Name = "Cmd02"
+        Me.Cmd02.Size = New System.Drawing.Size(88, 45)
+        Me.Cmd02.TabIndex = 91
+        Me.Cmd02.TabStop = False
+        Me.Cmd02.UseVisualStyleBackColor = False
+        '
+        'Cmd01
+        '
+        Me.Cmd01.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd01.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd01.ForeColor = System.Drawing.Color.Black
+        Me.Cmd01.Location = New System.Drawing.Point(3, 12)
+        Me.Cmd01.Name = "Cmd01"
+        Me.Cmd01.Size = New System.Drawing.Size(88, 45)
+        Me.Cmd01.TabIndex = 90
+        Me.Cmd01.TabStop = False
+        Me.Cmd01.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Cmd01.UseVisualStyleBackColor = False
+        '
+        'GroupBox10
+        '
+        Me.GroupBox10.Controls.Add(Me.btnDel01)
+        Me.GroupBox10.Controls.Add(Me.btnSel00)
+        Me.GroupBox10.Location = New System.Drawing.Point(3, 63)
+        Me.GroupBox10.Name = "GroupBox10"
+        Me.GroupBox10.Size = New System.Drawing.Size(1063, 450)
+        Me.GroupBox10.TabIndex = 102
+        Me.GroupBox10.TabStop = False
+        '
+        'btnSel00
+        '
+        Me.btnSel00.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnSel00.Font = New System.Drawing.Font("メイリオ", 9.75!, System.Drawing.FontStyle.Bold)
+        Me.btnSel00.Location = New System.Drawing.Point(6, 11)
+        Me.btnSel00.Name = "btnSel00"
+        Me.btnSel00.Size = New System.Drawing.Size(89, 36)
+        Me.btnSel00.TabIndex = 0
+        Me.btnSel00.Text = "全て選択"
+        Me.btnSel00.UseVisualStyleBackColor = False
+        '
+        'btnDel01
+        '
+        Me.btnDel01.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnDel01.Font = New System.Drawing.Font("メイリオ", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btnDel01.Location = New System.Drawing.Point(96, 11)
+        Me.btnDel01.Name = "btnDel01"
+        Me.btnDel01.Size = New System.Drawing.Size(89, 36)
+        Me.btnDel01.TabIndex = 1
+        Me.btnDel01.Text = "取消"
+        Me.btnDel01.UseVisualStyleBackColor = False
+        '
+        'GroupBox20
+        '
+        Me.GroupBox20.Controls.Add(Me.txtMsg)
+        Me.GroupBox20.Controls.Add(Me.btnDel00)
+        Me.GroupBox20.Controls.Add(Me.btnUP00)
+        Me.GroupBox20.Location = New System.Drawing.Point(3, 519)
+        Me.GroupBox20.Name = "GroupBox20"
+        Me.GroupBox20.Size = New System.Drawing.Size(1063, 83)
+        Me.GroupBox20.TabIndex = 103
+        Me.GroupBox20.TabStop = False
+        '
+        'btnUP00
+        '
+        Me.btnUP00.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnUP00.Font = New System.Drawing.Font("メイリオ", 9.75!, System.Drawing.FontStyle.Bold)
+        Me.btnUP00.Location = New System.Drawing.Point(6, 10)
+        Me.btnUP00.Name = "btnUP00"
+        Me.btnUP00.Size = New System.Drawing.Size(120, 36)
+        Me.btnUP00.TabIndex = 0
+        Me.btnUP00.Text = "経理仕訳一覧表"
+        Me.btnUP00.UseVisualStyleBackColor = False
+        '
+        'btnDel00
+        '
+        Me.btnDel00.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnDel00.Font = New System.Drawing.Font("メイリオ", 9.75!, System.Drawing.FontStyle.Bold)
+        Me.btnDel00.Location = New System.Drawing.Point(556, 10)
+        Me.btnDel00.Name = "btnDel00"
+        Me.btnDel00.Size = New System.Drawing.Size(110, 36)
+        Me.btnDel00.TabIndex = 1
+        Me.btnDel00.Text = "受信データ削除"
+        Me.btnDel00.UseVisualStyleBackColor = False
+        '
+        'txtMsg
+        '
+        Me.txtMsg.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtMsg.Font = New System.Drawing.Font("メイリオ", 9.75!)
+        Me.txtMsg.ForeColor = System.Drawing.Color.Red
+        Me.txtMsg.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.txtMsg.Location = New System.Drawing.Point(10, 49)
+        Me.txtMsg.Multiline = True
+        Me.txtMsg.Name = "txtMsg"
+        Me.txtMsg.ReadOnly = True
+        Me.txtMsg.Size = New System.Drawing.Size(341, 27)
+        Me.txtMsg.TabIndex = 91
+        Me.txtMsg.TabStop = False
+        Me.txtMsg.Text = "エラーメッセージ"
+        '
+        'F_An_Siw10
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1070, 614)
+        Me.Controls.Add(Me.GroupBox20)
+        Me.Controls.Add(Me.GroupBox10)
+        Me.Controls.Add(Me.Cmd12)
+        Me.Controls.Add(Me.Cmd11)
+        Me.Controls.Add(Me.Cmd10)
+        Me.Controls.Add(Me.Cmd08)
+        Me.Controls.Add(Me.Cmd07)
+        Me.Controls.Add(Me.Cmd06)
+        Me.Controls.Add(Me.Cmd09)
+        Me.Controls.Add(Me.Cmd04)
+        Me.Controls.Add(Me.Cmd05)
+        Me.Controls.Add(Me.Cmd03)
+        Me.Controls.Add(Me.Cmd02)
+        Me.Controls.Add(Me.Cmd01)
+        Me.Name = "F_An_Siw10"
+        Me.Text = "F_An_Siw10"
+        Me.GroupBox10.ResumeLayout(False)
+        Me.GroupBox20.ResumeLayout(False)
+        Me.GroupBox20.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Cmd12 As Button
+    Friend WithEvents Cmd11 As Button
+    Friend WithEvents Cmd10 As Button
+    Friend WithEvents Cmd08 As Button
+    Friend WithEvents Cmd07 As Button
+    Friend WithEvents Cmd06 As Button
+    Friend WithEvents Cmd09 As Button
+    Friend WithEvents Cmd04 As Button
+    Friend WithEvents Cmd05 As Button
+    Friend WithEvents Cmd03 As Button
+    Friend WithEvents Cmd02 As Button
+    Friend WithEvents Cmd01 As Button
+    Friend WithEvents GroupBox10 As GroupBox
+    Friend WithEvents btnDel01 As Button
+    Friend WithEvents btnSel00 As Button
+    Friend WithEvents GroupBox20 As GroupBox
+    Friend WithEvents btnDel00 As Button
+    Friend WithEvents btnUP00 As Button
+    Friend WithEvents txtMsg As TextBox
+End Class
---
+++ F_An_Siw10.vb
@@ -0,0 +1,5 @@
+﻿Public Class F_An_Siw10
+    Private Sub GroupBox20_Enter(sender As Object, e As EventArgs) Handles GroupBox20.Enter
+
+    End Sub
+End Class
