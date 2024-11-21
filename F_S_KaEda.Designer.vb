72def07837ed4434a771ce74ea729270f998ec23
felulu427 <toba427@icloud.com>
2024-11-21 22:05:06 +0900

    F_S_KaEda画面完成

---
+++ F_S_KaEda.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class F_S_KaEda
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.Cmd10 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListView1
+        '
+        Me.ListView1.HideSelection = False
+        Me.ListView1.Location = New System.Drawing.Point(3, 50)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(285, 388)
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'Cmd10
+        '
+        Me.Cmd10.BackColor = System.Drawing.SystemColors.Control
+        Me.Cmd10.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Cmd10.ForeColor = System.Drawing.Color.Black
+        Me.Cmd10.Location = New System.Drawing.Point(200, 2)
+        Me.Cmd10.Name = "Cmd10"
+        Me.Cmd10.Size = New System.Drawing.Size(80, 45)
+        Me.Cmd10.TabIndex = 135
+        Me.Cmd10.TabStop = False
+        Me.Cmd10.Text = "F10 " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "閉じる"
+        Me.Cmd10.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Cmd10.UseVisualStyleBackColor = False
+        '
+        'F_S_KaEda
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(289, 450)
+        Me.Controls.Add(Me.Cmd10)
+        Me.Controls.Add(Me.ListView1)
+        Me.Name = "F_S_KaEda"
+        Me.Text = "科目枝番参照"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents Cmd10 As Button
+End Class
---
+++ F_S_KaEda.vb
@@ -0,0 +1,9 @@
+﻿Public Class F_S_KaEda
+    Private Sub F_S_KaEda_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Cmd10_Click(sender As Object, e As EventArgs) Handles Cmd10.Click
+
+    End Sub
+End Class
